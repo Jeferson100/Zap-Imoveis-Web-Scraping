e16ce6d5579dd45311caf5476fead7419b913681
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-04-28 10:06:04 -0300

    Criando parelelo para cada cidade e cada fonte para olx joinville

--- codigos_rodando/joinville/joinville_coleta_dados_olx_0_70.py
+++ codigos_rodando/joinville/joinville_coleta_dados_olx_0_70.py
@@ -6,19 +6,9 @@ import time
 
 import os
 
-sys.path.append(str(Path(__file__).parent.parent))
-
-from unificando_dados import consolidar_jsons, consolidar_parquet
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
-import logging
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-logger = logging.getLogger(__name__)
 
 # Paths
 BASE_DIR    = Path(__file__).parent.parent.parent 
@@ -31,7 +21,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 from scraping_zap_imoveis import OLXColeta
 
 #URL_TEMPLATE = "https://www.olx.com.br/imoveis/venda/estado-sc/norte-de-santa-catarina/joinville?q=casa&o={pagina}"
-URL_TEMPLATE = "https://www.olx.com.br/imoveis/venda/estado-sc?q=joinville&ss=151&se=15000000&o={pagina}"
+URL_TEMPLATE = "https://www.olx.com.br/imoveis/venda/estado-sc?q=joinville&se=70&o={pagina}"
 
 sys.path.append('..')
     
@@ -39,7 +29,7 @@ now = time.strftime("%Y-%m")
 
 total_paginas = 100
 
-output_file   = PASTA_DADOS / f'{cidade}_olx_{now}_151_infinito.json' 
+output_file   = PASTA_DADOS / f'{cidade}_olx_{now}_0_70.json' 
 
 headless = os.getenv("HEADLESS", "True").lower() == "true"
 
@@ -51,4 +41,3 @@ resultado = asyncio.run(orchestrator.run(
     output_file=str(output_file),
     total_pages=total_paginas
 ))
-
