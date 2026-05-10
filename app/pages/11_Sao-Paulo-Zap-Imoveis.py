db1d984dd824526a84648d671430c9d14bcaa7f3
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-05-10 10:59:21 -0300

    Arrumando actions em sao paulo moema

--- app/pages/11_Sao-Paulo-Zap-Imoveis.py
+++
@@ -1,9 +0,0 @@
-from app_streamlit import gerar_pagina_analise_imoveis
-
-cidade_nome = 'Sao Paulo'
-
-prefixo_name = 'sao_paulo'
-
-cidade_path = 'sao_paulo'
-
-gerar_pagina_analise_imoveis(cidade_path, cidade_nome, prefixo_name)
