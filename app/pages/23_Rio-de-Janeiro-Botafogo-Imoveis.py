2cf33c48547c02bb0e6f476b313edf014f69bac5
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-05-18 16:08:29 -0300

    Criando copacabana

---
+++ app/pages/23_Rio-de-Janeiro-Botafogo-Imoveis.py
@@ -0,0 +1,9 @@
+from app_streamlit import gerar_pagina_analise_imoveis
+
+cidade_nome = 'Rio de Janeiro - Botafogo'
+
+prefixo_name = 'rio_janeiro_botafogo'
+
+cidade_path = 'rio_janeiro/botafogo'
+
+gerar_pagina_analise_imoveis(cidade_path, cidade_nome, prefixo_name)
---
+++ app/pages/25_Rio-de-Janeiro-Ipanema-Imoveis.py
@@ -0,0 +1,9 @@
+from app_streamlit import gerar_pagina_analise_imoveis
+
+cidade_nome = 'Rio de Janeiro - Ipanema'
+
+prefixo_name = 'rio_janeiro_ipanema'
+
+cidade_path = 'rio_janeiro/ipanema'
+
+gerar_pagina_analise_imoveis(cidade_path, cidade_nome, prefixo_name)
