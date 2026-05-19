9eb90f43977b0a9db60f553d20431cf3a2acb462
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-05-19 10:02:48 -0300

    Criando flamengo action

---
+++ app/pages/27_Rio-de-Janeiro-Copacabana-Imoveis.py
@@ -0,0 +1,9 @@
+from app_streamlit import gerar_pagina_analise_imoveis
+
+cidade_nome = 'Rio de Janeiro - Copacabana'
+
+prefixo_name = 'rio_janeiro_copacabana'
+
+cidade_path = 'rio_janeiro/copacabana'
+
+gerar_pagina_analise_imoveis(cidade_path, cidade_nome, prefixo_name)
