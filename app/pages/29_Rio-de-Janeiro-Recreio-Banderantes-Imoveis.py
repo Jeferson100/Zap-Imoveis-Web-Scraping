11e55d6cf21b614aeb8babff5acc417e5abf990f
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-05-19 16:41:56 -0300

    Criando action tijuca

---
+++ app/pages/29_Rio-de-Janeiro-Recreio-Banderantes-Imoveis.py
@@ -0,0 +1,9 @@
+from app_streamlit import gerar_pagina_analise_imoveis
+
+cidade_nome = 'Rio de Janeiro - Recreio dos Banderantes'
+
+prefixo_name = 'rio_janeiro_recreio-banderantes'
+
+cidade_path = 'rio_janeiro/recreio-banderantes'
+
+gerar_pagina_analise_imoveis(cidade_path, cidade_nome, prefixo_name)
---
+++ app/pages/30_Rio-de-Janeiro-Tijuca-Imoveis.py
@@ -0,0 +1,9 @@
+from app_streamlit import gerar_pagina_analise_imoveis
+
+cidade_nome = 'Rio de Janeiro - Tijuca'
+
+prefixo_name = 'rio_janeiro_tijuca'
+
+cidade_path = 'rio_janeiro/tijuca'
+
+gerar_pagina_analise_imoveis(cidade_path, cidade_nome, prefixo_name)
