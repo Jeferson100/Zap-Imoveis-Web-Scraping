0c45671fd5c45105ce534460b5cb414b3648c564
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-05-22 09:20:16 -0300

    Arrumando limpeza pinheiros

--- app/pages/29_Rio-de-Janeiro-Recreio-Banderantes-Imoveis.py
+++ app/pages/29_Rio-de-Janeiro-Recreio-Banderantes-Imoveis.py
@@ -1,9 +1,9 @@
 from app_streamlit import gerar_pagina_analise_imoveis
 
-cidade_nome = 'Rio de Janeiro - Recreio dos Banderantes'
+cidade_nome = 'Rio de Janeiro - Recreio dos Bandeirantes'
 
-prefixo_name = 'rio_janeiro_recreio-banderantes'
+prefixo_name = 'rio_janeiro_recreio-bandeirantes'
 
-cidade_path = 'rio_janeiro/recreio-banderantes'
+cidade_path = 'rio_janeiro/recreio-bandeirantes'
 
 gerar_pagina_analise_imoveis(cidade_path, cidade_nome, prefixo_name)
