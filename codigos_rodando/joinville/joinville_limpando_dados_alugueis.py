d68f0e9054e08a89d6d4d204ec500fb2e3aa89ef
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-08-14 18:27:29 -0300

    Update mensal Joinville: 14/08/2026

---
+++ codigos_rodando/joinville/joinville_limpando_dados_alugueis.py
@@ -0,0 +1,61 @@
+from unicodedata import name
+from pathlib import Path
+import logging
+import warnings
+import sys
+import os
+from dotenv import load_dotenv
+load_dotenv()
+sys.path.append(str(Path(__file__).parent.parent))
+from limpando_dados import limpando_dados
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+warnings.filterwarnings("ignore")
+
+cidade = os.getenv("CIDADE_PASTA")
+
+cidade_limpeza      =  os.getenv("CIDADE_LIMPEZA")
+
+cidade_localizacao  =  os.getenv("CIDADE_LOCALIZACAO")
+
+estado_limpeza      =  os.getenv("ESTADO_LIMPEZA")
+
+estado_localizacao  =  os.getenv("ESTADO_LOCALIZACAO")
+
+PASTA_DADOS  = Path(__file__).parent.parent.parent / 'dados'/ cidade
+
+BATCH  = os.getenv("BATCH_LIMPEZA")
+
+BATCH = int(BATCH) if BATCH and BATCH.isdigit() else 100
+
+logger.info(f"Iniciando limpeza de dados de aluguel de imóveis de {cidade}...")
+
+logger.info(f"Pasta de dados: {PASTA_DADOS}")
+
+PASTA_DADOS.mkdir(parents=True, exist_ok=True)
+
+logger.info(f"Os parametros de limpeza são: cidade_limpeza={cidade_limpeza}, cidade_localizacao={cidade_localizacao}, estado_limpeza={estado_limpeza}, estado_localizacao={estado_localizacao}, BATCH={BATCH}")
+
+MAPA_BAIRROS = {
+        'pirabeiraba': ['pirabeiraba', 'dona francisca', 'distrito industrial norte'],
+        'distrito industrial': ['distrito industrial', 'zona industrial', 'distrito industrial norte', 'distrito industrial sul'],
+        'area rural': ['area rural', 'rural'],
+        'vila nova': ['vila nova'], 
+        's/b': ['localizacao'], 
+    }
+
+limpando_dados(name_arquivo_zap = f'{cidade}_aluguel_zap_*.parquet', 
+               name_arquivo_vivareal = f'{cidade}_aluguel_vivareal_*.parquet', 
+               name_arquivo_chave_mao = f'{cidade}_aluguel_chave_mao_*.parquet',
+               name_arquivo_olx = f'{cidade}_aluguel_olx_*.json',
+               name_arquivo_saida = f'{cidade}_aluguel_imoveis_limpo', 
+               pasta_dados = PASTA_DADOS, 
+               tipo_async = True,
+               batch = BATCH, 
+               cidade_limpeza=cidade_limpeza,
+               cidade_localizacao=cidade_localizacao,
+               estado_limpeza=estado_limpeza,
+               estado_localizacao=estado_localizacao, 
+               MAPA_BAIRROS=MAPA_BAIRROS,
+               )
