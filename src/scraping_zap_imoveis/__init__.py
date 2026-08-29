9f14e4f48de67b90a3f098bed8eec0d265ed4e01
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-08-29 13:30:19 -0300

    Adcionando coleta joinville imovelweb

--- src/scraping_zap_imoveis/__init__.py
+++ src/scraping_zap_imoveis/__init__.py
@@ -3,6 +3,10 @@ from .link_anuncios_imovelweb_playwright_async import ImovelWebScraperLinksAsync
 from .extrair_dados_imovelweb_playwright_async import ImovelWebDadosImovelAsync, DadosImovelImovelWeb
 from .total_page_imovelweb import TotalPageImovelWeb
 from .imovelweb_coleta import ImovelWebColeta
+from .chave_mao_coleta import ChavesMaoColeta
+from .olx_coleta import OLXColeta
+from .zap_imoveis_coleta import ZapImoveisColeta
+from .viva_real_coleta import VivaRealColeta
 
 __all__ = [
     "ImovelWebScraperLinksAsync",
@@ -10,4 +14,8 @@ __all__ = [
     "DadosImovelImovelWeb",
     "TotalPageImovelWeb",
     "ImovelWebColeta",
+    "ChavesMaoColeta",
+    "OLXColeta",
+    "ZapImoveisColeta",
+    "VivaRealColeta"
 ]
