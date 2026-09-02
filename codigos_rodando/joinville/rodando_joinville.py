156dba7a12d425fc27c1d288e3e0bb85c0575710
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-09-01 21:48:04 -0300

    Update mensal Joinville: 01/09/2026

--- codigos_rodando/joinville/rodando_joinville.py
+++ codigos_rodando/joinville/rodando_joinville.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
    
     fluxo = [
         #(f"uv run {cidade}_coleta_dados_zap_imoveis.py", "Scraper Zap Imóveis"),
-        #(f"uv run {cidade}_coleta_dados_viva_real.py", "Scraper Viva Real"),
+        (f"uv run {cidade}_coleta_dados_viva_real.py", "Scraper Viva Real"),
         #(f"uv run {cidade}_coleta_dados_chave_mao.py", "Scraper Chave na Mão"),
         #(f"uv run {cidade}_coleta_dados_olx_0_67.py", "Scraper OLX"),
         #(f"uv run {cidade}_coleta_dados_olx_68_90.py", "Scraper OLX"),
@@ -37,9 +37,9 @@ if __name__ == "__main__":
         #(f"uv run {cidade}_limpando_dados_imoveis.py", "Processamento de Dados"),
         #(f"uv run {cidade}_criando_indice_localizacao.py", "Criação do Índice de Localização")
         #(f"uv run {cidade}_coleta_alugueis_chave_mao.py", "Scraper Chave na Mão"),
-        (f"uv run {cidade}_coleta_dados_imovelweb_0_100.py", "Scraper Imovelweb"),
-        (f"uv run {cidade}_coleta_dados_imovelweb_101_200.py", "Scraper Imovelweb"),
-        (f"uv run {cidade}_coleta_dados_imovelweb_201_infinito.py", "Scraper Imovelweb"),
+        #(f"uv run {cidade}_coleta_dados_imovelweb_0_100.py", "Scraper Imovelweb"),
+        #(f"uv run {cidade}_coleta_dados_imovelweb_101_200.py", "Scraper Imovelweb"),
+        #(f"uv run {cidade}_coleta_dados_imovelweb_201_infinito.py", "Scraper Imovelweb"),
         #(f"uv run {cidade}_coleta_alugueis_zap.py", "Scraper Zap Imóveis"),
         #(f"uv run {cidade}_coleta_alugueis_vivareal.py", "Scraper Viva Real"),
         #(f"uv run {cidade}_juncao_dados_aluguel.py", "Junção de Dados de Aluguel"),
