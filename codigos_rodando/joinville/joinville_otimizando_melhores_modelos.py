907cbf7efcaf105068816abf01eadfb339f2cb00
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2026-09-19 16:33:56 -0300

    Mudando to_k otimizacao joinville de 6 para 3

--- codigos_rodando/joinville/joinville_otimizando_melhores_modelos.py
+++ codigos_rodando/joinville/joinville_otimizando_melhores_modelos.py
@@ -22,13 +22,19 @@ warnings.filterwarnings("ignore")
 cidade = os.getenv("CIDADE_PASTA")
 
 MES_REF = os.getenv("MES_REF", datetime.now().strftime("%Y-%m"))
+
 cidade_nome = os.getenv("LOCALIZAZAO_COMPLETA", "Joinville, Santa Catarina, Brasil")
 
 N_TRIALS = int(os.getenv("N_TRIALS_OPTUNA", "300"))
+
 SELECTION_MODE = os.getenv("SELECTION_MODE", "combinado")
-TOP_K = int(os.getenv("TOP_K", "6"))
+
+TOP_K = int(os.getenv("TOP_K", "3"))
+
 TOP_K_MODELO = int(os.getenv("TOP_K_MODELO", "1"))
+
 INCLUIR_MLP = os.getenv("INCLUIR_MLP", "true").lower() == "true"
+
 INCLUIR_TOPICOS = os.getenv("INCLUIR_TOPICOS", "True").lower() == "true"
 
 PASTA_DADOS = Path(__file__).parent.parent.parent / 'dados' / cidade
